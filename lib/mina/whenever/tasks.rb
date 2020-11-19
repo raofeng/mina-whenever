18dded035d1fa1a4f29f7376a12bee0b0afa41c1
Ray Rao <raofeng@tengxincar.cn>
2020-11-19 16:58:37 +0800

    Update

--- lib/mina/whenever/tasks.rb
+++ lib/mina/whenever/tasks.rb
@@ -5,25 +5,28 @@ set :whenever_name, -> { "#{fetch(:application_name)}_#{fetch(:rails_env)}" }
 namespace :whenever do
   desc 'Clear crontab'
   task clear: :remote_environment do
-    comment "Clear contrab for #{fetch(:whenever_name)}"
+    comment "-----Clear contrab for #{fetch(:whenever_name)}-----"
+    comment "-----#{fetch(:whenever_environment_variable)}=#{fetch(:rails_env)}-----"
     in_path fetch(:current_path) do
-      command "#{fetch(:whenever_environment_variable)}=#{fetch(:rails_env)} #{fetch(:bundle_bin)} exec whenever --clear-crontab #{fetch(:whenever_name)}"
+      command "#{fetch(:whenever_environment_variable)}=#{fetch(:rails_env)} #{fetch(:bundle_bin)} exec whenever --clear-crontab #{fetch(:whenever_name)} --set 'environment=#{fetch(:rails_env)}&path=#{fetch(:current_path)}'"
     end
   end
 
   desc 'Update crontab'
   task update: :remote_environment do
-    comment "Update crontab for #{fetch(:whenever_name)}"
+    comment "-----Update crontab for #{fetch(:whenever_name)}-----"
+    comment "-----#{fetch(:whenever_environment_variable)}=#{fetch(:rails_env)}-----"
     in_path fetch(:current_path) do
-      command "#{fetch(:whenever_environment_variable)}=#{fetch(:rails_env)} #{fetch(:bundle_bin)} exec whenever --update-crontab #{fetch(:whenever_name)}"
+      command "#{fetch(:whenever_environment_variable)}=#{fetch(:rails_env)} #{fetch(:bundle_bin)} exec whenever --update-crontab #{fetch(:whenever_name)} --set 'environment=#{fetch(:rails_env)}&path=#{fetch(:current_path)}'"
     end
   end
 
   desc 'Write crontab'
   task write: :remote_environment do
-    comment "Write crontab for #{fetch(:whenever_name)}"
+    comment "-----Write crontab for #{fetch(:whenever_name)}-----"
+    comment "-----#{fetch(:whenever_environment_variable)}=#{fetch(:rails_env)}-----"
     in_path fetch(:current_path) do
-      command "#{fetch(:whenever_environment_variable)}=#{fetch(:rails_env)} #{fetch(:bundle_bin)} exec whenever --write-crontab #{fetch(:whenever_name)}"
+      command "#{fetch(:whenever_environment_variable)}=#{fetch(:rails_env)} #{fetch(:bundle_bin)} exec whenever --write-crontab #{fetch(:whenever_name)} --set 'environment=#{fetch(:rails_env)}&path=#{fetch(:current_path)}'"
     end
   end
 end
